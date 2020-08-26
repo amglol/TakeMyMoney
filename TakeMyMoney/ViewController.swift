744ec892afec7f173bc0e7b26abd1e56628ed6c0
adiran <avg5074@gmail.com>
2020-08-25 22:46:16 -0400

    initial commit

---
+++ TakeMyMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeMyMoney
+//
+//  Created by Adrian Galecki on 8/25/20.
+//  Copyright © 2020 Adrian Galecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
