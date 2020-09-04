e7ca79ea120891d1cab34572e19ee0a67ad006f8
adiran <avg5074@gmail.com>
2020-09-03 23:25:34 -0400

    finished up with the payment model, need to add a checker if any of the fields are empty - then block the proceed to payment button and show where the error is

--- TakeMyMoney/Controller/PaymentSelectionTableVC.swift
+++ TakeMyMoney/Controller/PaymentSelectionTableVC.swift
@@ -13,19 +13,69 @@ class PaymentSelectionTableVC: UITableViewController {
     @IBOutlet weak var creditCardInputField: UITextField!
     @IBOutlet weak var validUntilInputField: UITextField!
     @IBOutlet weak var cvvInputField: UITextField!
+    @IBOutlet weak var nameInputfield: UITextField!
+    
+    //payment buttons
+    @IBOutlet weak var paypalBtnOutlet: UIButton!
+    @IBOutlet weak var creditCardBtnOutlet: UIButton!
+    
+    var isLimitReached = false
+    var paypalSelected = false
+    var creditCardSelected = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-    @IBAction func creditCardTextFieldValueChanged(_ sender: UITextField) {
-        guard let creditCardNumber = creditCardInputField.text else { return }
-        let isLimitedReached = checkInputFieldLength(inputType: InputFieldType.CARD_NUM, inputValue: creditCardNumber)
-        
-        if isLimitedReached {
-            createCustomAlert(alertTitle: "Limit Reached", message: "You are attempting to enter invalid digits.")
-            creditCardInputField.text = ""
+    @IBAction func proceedToPayment(_ sender: UIButton) {
+        let paymentValues = payment
+        print("The payment details are: \(payment)")
+    }
+    
+    @IBAction func paymentButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            UpdateButtonView(enterTheButtonOutlet: paypalBtnOutlet)
+        case 2:
+            UpdateButtonView(enterTheButtonOutlet: creditCardBtnOutlet)
+        default:
+            print("error")
+        }
+    }
+    
+    @IBAction func inputFieldValueChanged(_ sender: UITextField) {
+        switch sender {
+        case creditCardInputField:
+            guard let creditCardNumber = creditCardInputField.text else { return }
+            isLimitReached = checkInputFieldLength(inputType: InputFieldType.CARD_NUM, inputValue: creditCardNumber)
+            
+            if isLimitReached {
+                createCustomAlert(alertTitle: "Limit Reached", message: "You are attempting to enter invalid digits.")
+                creditCardInputField.text = ""
+            }
+        case validUntilInputField:
+            guard let validUntilNumber = validUntilInputField.text else { return }
+            isLimitReached = checkInputFieldLength(inputType: InputFieldType.VALID_UNTIL, inputValue: validUntilNumber)
+            
+            if validUntilNumber.count == 2 {
+                validUntilInputField.text = validUntilNumber + "/"
+            }
+            
+            if isLimitReached {
+                createCustomAlert(alertTitle: "Limit Reached", message: "You are attempting to enter invalid digits.")
+                validUntilInputField.text = ""
+            }
+        case cvvInputField:
+            guard let cvvNumber = cvvInputField.text else { return }
+            isLimitReached = checkInputFieldLength(inputType: InputFieldType.CVV_NUM, inputValue: cvvNumber)
+            
+            if isLimitReached {
+                createCustomAlert(alertTitle: "Limit Reached", message: "You are attempting to enter invalid digits.")
+                cvvInputField.text = ""
+            }
+        default:
+            print("error")
         }
     }
     
@@ -38,17 +88,57 @@ class PaymentSelectionTableVC: UITableViewController {
                 isLimitReached = true
             }
         case .CVV_NUM:
-            if inputValue.count > 4 {
+            if inputValue.count > 3 {
                 isLimitReached = true
             }
         case .VALID_UNTIL:
             if inputValue.count > 7 {
                 isLimitReached = true
             }
-        default:
-            print("Error")
         }
         return isLimitReached
+    }
+    
+    func UpdateButtonView(enterTheButtonOutlet btnOutlet: UIButton) {
+        switch btnOutlet {
+        case paypalBtnOutlet:
+            paypalSelected = true
+            creditCardSelected = false
+        case creditCardBtnOutlet:
+            paypalSelected = false
+            creditCardSelected = true
+        default:
+            print("error")
+        }
+        
+        if paypalSelected {
+            paypalBtnOutlet.backgroundColor = #colorLiteral(red: 0.476841867, green: 0.5048075914, blue: 1, alpha: 1)
+            creditCardBtnOutlet.backgroundColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
+        }
+        else {
+            paypalBtnOutlet.backgroundColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
+            creditCardBtnOutlet.backgroundColor = #colorLiteral(red: 0.476841867, green: 0.5048075914, blue: 1, alpha: 1)
+        }
+    }
+    
+    var payment: PaymentInformation? {
+        
+        let totalPrice = 5950.00
+        let creditCardNum = creditCardInputField.text ?? ""
+        let expirationDate = validUntilInputField.text ?? ""
+        let cvvNumber = cvvInputField.text ?? ""
+        let fullName = nameInputfield.text ?? ""
+        
+        var paymentMethod = PaymentMethod.ERROR
+        
+        if paypalSelected {
+            paymentMethod = PaymentMethod.PAYPAL
+        }
+        else {
+            paymentMethod = PaymentMethod.CREDIT_CARD
+        }
+        
+        return PaymentInformation(totalPrice: totalPrice, creditCard: creditCardNum, expirationDate: expirationDate, cvvNumber: cvvNumber, fullName: fullName, paymentMethod: paymentMethod)
     }
     
     func createCustomAlert(alertTitle: String, message: String) {
@@ -57,6 +147,10 @@ class PaymentSelectionTableVC: UITableViewController {
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
         
         self.present(alert, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
--- TakeMyMoney/Model/PaymentHelper.swift
+++ TakeMyMoney/Model/PaymentHelper.swift
@@ -11,3 +11,25 @@ import Foundation
 enum InputFieldType {
     case CARD_NUM, VALID_UNTIL, CVV_NUM
 }
+
+enum PaymentMethod {
+    case PAYPAL, CREDIT_CARD, ERROR
+}
+
+struct PaymentInformation {
+    private(set) public var totalPrice: Double
+    private(set) public var creditCard: String
+    private(set) public var expirationDate: String
+    private(set) public var cvvNumber: String
+    private(set) public var fullName: String
+    private(set) public var paymentMethod: PaymentMethod
+    
+    init(totalPrice: Double, creditCard: String, expirationDate: String, cvvNumber: String, fullName: String, paymentMethod: PaymentMethod) {
+        self.totalPrice = totalPrice
+        self.creditCard = creditCard
+        self.expirationDate = expirationDate
+        self.cvvNumber = cvvNumber
+        self.fullName = fullName
+        self.paymentMethod = paymentMethod
+    }
+}
